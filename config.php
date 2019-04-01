0eb48bb603c617a642dac00e3e167e2f67b15bf9
Jose Montilla <jose.montilla@gmail.com>
2019-04-01 11:25:32 -0400

    Update config.php

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
-$dbhost = '192.168.159.59';
-$dbuser = 'newwebuser';
-$dbpass = 'cENt4@35';
+$dbhost = '';
+$dbuser = '';
+$dbpass = '';
 
 $conn = mysql_connect($dbhost, $dbuser, $dbpass) or die ('Error connecting to mysql');
 
-$dbname = 'gradschoolusadev';
+$dbname = '';
 mysql_select_db($dbname);
 ?>
